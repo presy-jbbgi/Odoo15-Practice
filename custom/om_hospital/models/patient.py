1aecc6ff3626c03c0bf5b2703daebd4b0c0a91b8
presy-jbbgi <presy@jbbgi.com>
2022-02-03 16:29:55 +0800

    update

--- custom/om_hospital/models/patient.py
+++ custom/om_hospital/models/patient.py
@@ -6,13 +6,20 @@ from odoo import models, fields, api
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string="Name", required="True")
     age = fields.Integer(string="Age")
     gender = fields.Selection(
-        [('male', 'Male'), ('female', 'Female'),], required=True, default='male'
+        [('male', 'Male'), ('female', 'Female'), ], required=True, default='male'
     )
     note = fields.Text(string="Description")
+    state = fields.Selection(selection=[
+        ('draft', 'Draft'),
+        ('confirm', 'Confirmed'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled'),
+    ], string='Status', default='draft')
 #
 #     @api.depends('value')
 #     def _value_pc(self):
