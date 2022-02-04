27a0a4f28a7ae8417a6139d8bbc150b21d4e92da
presy-jbbgi <presy@jbbgi.com>
2022-02-04 15:01:45 +0800

    update

--- custom/om_hospital/models/patient.py
+++ custom/om_hospital/models/patient.py
@@ -8,24 +8,20 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name = fields.Char(string="Name", required="True")
-    age = fields.Integer(string="Age")
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age", tracking=True)
     gender = fields.Selection(
         [('male', 'Male'), ('female', 'Female'), ], required=True, default='male'
     )
-    note = fields.Text(string="Description")
+    note = fields.Text(string="Description", tracking=True)
     state = fields.Selection(selection=[
         ('draft', 'Draft'),
         ('confirm', 'Confirmed'),
         ('done', 'Done'),
         ('cancel', 'Cancelled'),
-    ], string='Status', default='draft')
+    ], string='Status', default='draft', tracking=True)
+    responsible_id = fields.Many2one('res.partner', string="Parent")
 
-    #
-    #     @api.depends('value')
-    #     def _value_pc(self):
-    #         for record in self:
-    #             record.value2 = float(record.value) / 100
     def to_confirm(self):
         # self.state = 'confirmed'
         self.write({'state': 'confirm'})
@@ -41,3 +37,9 @@ class HospitalPatient(models.Model):
     def to_draft(self):
         # self.state = 'confirmed'
         self.write({'state': 'draft'})
+
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
