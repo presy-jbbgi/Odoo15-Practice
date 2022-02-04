a8b2c807bad6a4775147344e584cdcfe2d7c6c4c
presy-jbbgi <presy@jbbgi.com>
2022-02-04 11:05:40 +0800

    update

--- custom/om_hospital/models/patient.py
+++ custom/om_hospital/models/patient.py
@@ -20,8 +20,24 @@ class HospitalPatient(models.Model):
         ('done', 'Done'),
         ('cancel', 'Cancelled'),
     ], string='Status', default='draft')
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+
+    #
+    #     @api.depends('value')
+    #     def _value_pc(self):
+    #         for record in self:
+    #             record.value2 = float(record.value) / 100
+    def to_confirm(self):
+        # self.state = 'confirmed'
+        self.write({'state': 'confirm'})
+
+    def to_done(self):
+        # self.state = 'confirmed'
+        self.write({'state': 'done'})
+
+    def to_cancel(self):
+        # self.state = 'confirmed'
+        self.write({'state': 'cancel'})
+
+    def to_draft(self):
+        # self.state = 'confirmed'
+        self.write({'state': 'draft'})
